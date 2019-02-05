ca76c071d09fe9066de856891a67bed256e775db
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-02-05 23:12:48 +0100

    Add code

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -35,4 +35,53 @@ class CategorieController extends AbstractController
             'categories' => $categories,
         ]);
     }
+
+    /**
+     * @Route("/{numcat}/")
+     */
+    public function edit(Request $request, $numcat)
+    {
+        $errors = [];
+        $categorie = [];
+
+        // y a-t-il des données envoyées en POST
+        if ($request->getMethod() == 'POST') {
+            // validation des données
+
+            // vérification si le champ a bien été transmis et qu'il n'est pas vide
+            if (!$request->request->has('libelle') || empty($request->request->get('libelle'))) {
+                // le champ n'a pas été transmis ou il est vide
+                $errors['libelle'] = 'Veuillez renseigner ce champ';
+            } else {
+                // le champ a pas été transmis et il n'est pas vide
+                $categorie['libelle'] = $request->request->get('libelle');
+            }
+
+            // vérification si le champ a bien été transmis et qu'il n'est pas vide
+            if (!$request->request->has('tauxtva') || empty($request->request->get('tauxtva'))) {
+                // le champ n'a pas été transmis ou il est vide
+                $errors['tauxtva'] = 'Veuillez renseigner ce champ';
+            } else {
+                // le champ a pas été transmis et il n'est pas vide
+                $categorie['tauxtva'] = $request->request->get('tauxtva');
+            }
+
+            if (!$errors) {
+                // il n'y a pas d'erreur, on peut mettre la BDD à jour
+
+                $count = $this->conn->update('categorie', [
+                    'libelle' => $categorie['libelle'],
+                    'tauxtva' => $categorie['tauxtva'],
+                ], ['numcat' => $numcat]);
+            }
+        }
+
+        $categorie = $this->conn->fetchAssoc('SELECT * FROM `categorie` WHERE `numcat` = :numcat', [
+            'numcat' => $numcat,
+        ]);
+
+        return $this->render('categorie/edit.html.twig', [
+            'categorie' => $categorie,
+        ]);
+    }
 }
