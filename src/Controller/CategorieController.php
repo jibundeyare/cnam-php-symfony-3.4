d903dc433e404c03132807cd3fbcbc0069d0ec17
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2019-02-14 00:29:24 +0100

    Add code

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -28,42 +28,122 @@ class CategorieController extends AbstractController
     {
         $sql = "SELECT * FROM categorie";
 
-        // envoi d'une requête SQL à la BDD et récupération du résultat sous forme de tableau PHP dans la variable `$items`
+        // envoi d'une requête SQL à la BDD et récupération du résultat sous forme d'un tableau de tableaux PHP dans la variable `$categories`
         $categories = $this->conn->fetchAll($sql);
 
+        // affichage du rendu d'un template
         return $this->render('categorie/index.html.twig', [
+            // transmission de données au template
             'categories' => $categories,
         ]);
     }
 
     /**
-     * @Route("/{numcat}/")
+     * @Route("/{numcat}/", name="categorie_show", requirements={"numcat"="\d+"})
+     */
+    public function show(Request $request, $numcat)
+    {
+        // envoi d'une requête SQL à la BDD et récupération du résultat sous forme d'un' tableau PHP dans la variable `$categorie`
+        $categorie = $this->conn->fetchAssoc('SELECT * FROM `categorie` WHERE `numcat` = :numcat', [
+            'numcat' => $numcat,
+        ]);
+
+        // affichage du rendu d'un template
+        return $this->render('categorie/show.html.twig', [
+            // transmission de données au template
+            'categorie' => $categorie,
+        ]);
+    }
+
+    /**
+     * @Route("/new/", name="categorie_new")
+     */
+    public function new(Request $request)
+    {
+        $errors = [];
+
+        $categorie = [
+            'numcat' => null,
+            'libelle' => null,
+            'tauxtva' => null,
+        ];
+
+        // y a-t-il des données envoyées en POST
+        if ($request->getMethod() == 'POST') {
+            $categorie['libelle'] = $request->request->get('libelle');
+            $categorie['tauxtva'] = $request->request->get('tauxtva');
+
+            // validation des données
+
+            // vérification si le champ a bien été transmis et qu'il n'est pas vide
+            if (empty($categorie['libelle'])) {
+                // le champ n'a pas été transmis ou il est vide
+                $errors['libelle'] = 'Veuillez renseigner ce champ';
+            }
+
+            // vérification si le champ a bien été transmis et qu'il n'est pas vide
+            if (empty($categorie['tauxtva'])) {
+                // le champ n'a pas été transmis ou il est vide
+                $errors['tauxtva'] = 'Veuillez renseigner ce champ';
+            }
+
+            if (!$errors) {
+                // il n'y a pas d'erreur, on peut mettre la BDD à jour
+
+                // exécution de la requête et récupération du nombre de lignes affectées dans la variable `$count`
+                $count = $this->conn->insert('categorie', [
+                    'libelle' => $categorie['libelle'],
+                    'tauxtva' => $categorie['tauxtva'],
+                ]);
+
+                // récupération de l'id de la dernière ligne créée par la BDD dans la variable `$lastInsertId`
+                $lastInsertId = $this->conn->lastInsertId();
+
+                // redirection vers la page de visualisation
+                return $this->redirectToRoute('categorie_show', [
+                    'numcat' => $lastInsertId
+                ]);
+            }
+        }
+
+        // affichage du rendu d'un template
+        return $this->render('categorie/new.html.twig', [
+            // transmission de données au template
+            'errors' => $errors,
+            'categorie' => $categorie,
+        ]);
+    }
+
+    // @todo créer une action suppression (seulement moi)
+
+    /**
+     * @Route("/{numcat}/edit", name="categorie_edit", requirements={"numcat"="\d+"})
      */
     public function edit(Request $request, $numcat)
     {
         $errors = [];
-        $categorie = [];
+
+        $categorie = $this->conn->fetchAssoc('SELECT * FROM `categorie` WHERE `numcat` = :numcat', [
+            'numcat' => $numcat,
+        ]);
 
         // y a-t-il des données envoyées en POST
         if ($request->getMethod() == 'POST') {
+            $categorie['libelle'] = $request->request->get('libelle');
+            $categorie['tauxtva'] = $request->request->get('tauxtva');
+
             // validation des données
 
             // vérification si le champ a bien été transmis et qu'il n'est pas vide
-            if (!$request->request->has('libelle') || empty($request->request->get('libelle'))) {
+            if (empty($categorie['libelle'])) {
                 // le champ n'a pas été transmis ou il est vide
                 $errors['libelle'] = 'Veuillez renseigner ce champ';
-            } else {
-                // le champ a pas été transmis et il n'est pas vide
-                $categorie['libelle'] = $request->request->get('libelle');
             }
 
             // vérification si le champ a bien été transmis et qu'il n'est pas vide
-            if (!$request->request->has('tauxtva') || empty($request->request->get('tauxtva'))) {
+            if (empty($categorie['tauxtva'])) {
                 // le champ n'a pas été transmis ou il est vide
                 $errors['tauxtva'] = 'Veuillez renseigner ce champ';
-            } else {
-                // le champ a pas été transmis et il n'est pas vide
-                $categorie['tauxtva'] = $request->request->get('tauxtva');
             }
 
             if (!$errors) {
@@ -76,11 +156,10 @@ class CategorieController extends AbstractController
             }
         }
 
-        $categorie = $this->conn->fetchAssoc('SELECT * FROM `categorie` WHERE `numcat` = :numcat', [
-            'numcat' => $numcat,
-        ]);
-
+        // affichage du rendu d'un template
         return $this->render('categorie/edit.html.twig', [
+            // transmission de données au template
+            'errors' => $errors,
             'categorie' => $categorie,
         ]);
     }
